f4f08592bde9741a2efb1372cfbf686b3ca4cadc
Daniel Jauregui <djvdjv209@gmail.com>
2024-10-27 22:53:29 -0700

    initializing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div class="bg-blue-500 text-white p-4">
-      This is a Tailwind CSS component!
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
